098c28c9152e6ecac487485c6530d30fbb390cd1
JacobWetzel <jkw5752@psu.edu>
2023-12-01 00:13:36 -0500

    non-production push, same code as previous but in .java not .class

--- DatabaseLogin.java
+++ DatabaseLogin.java
@@ -1,4 +1,14 @@
-public class main {
+import javax.swing.*;
 
-    
+
+
+public class DatabaseLogin {
+
+    public static void main(String[] args){
+        System.out.println("here\n");
+        LoginWindow loginWindow = new LoginWindow("LoginWindow");
+        return;
+    }
+
+
 }
--- LoginWindow.java
+++ LoginWindow.java
@@ -1,2 +1,17 @@
-public class LoginWindow {
+import javax.swing.*;
+import java.awt.event.*;
+public class LoginWindow extends JFrame implements ActionListener{
+
+    public LoginWindow(String frameTitle) {
+        super(frameTitle);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
 }
