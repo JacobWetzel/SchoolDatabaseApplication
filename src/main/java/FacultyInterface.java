bba59ccac80df1cbd892443919a4a5df9c23e05e
JacobWetzel <jkw5752@psu.edu>
2023-12-02 19:42:21 -0500

    This function makes it so that a facutly will see one screen whereas a student will see a different interface

---
+++ src/main/java/FacultyInterface.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FacultyInterface extends JFrame {
+    public FacultyInterface(){
+        setTitle("Faculty Login");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+}
